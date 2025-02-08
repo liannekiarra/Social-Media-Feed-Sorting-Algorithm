7a2c6db1a2dffb938fb5743e8c0119897547b21c
liannekiarra <liannekiarra@gmail.com>
2025-02-08 20:13:57 +0000

    Create OPP_template.py

---
+++ OPP_template.py
@@ -0,0 +1,115 @@
+import time
+import datetime
+from datetime import datetime
+
+
+#this algorithm can only sort a feed that is in the same month as the current date
+#it is being run.
+
+class User:
+    contentPreference = None
+    
+    def getFeed():
+        return
+    
+    def seeOlderPost():
+        return
+    
+    def seeNewPosts():
+        return
+    
+    def seeImagePost():
+        return
+    
+    def seeVideoClipPosts():
+        return
+
+
+class FeedGenerator:
+    def buildPost():
+        
+        return
+
+class SortEngagementScore:
+    def sortScore():
+        
+        
+        return
+    
+class Feed:
+    
+    def Feed():
+        #creating feed for a user.
+        # FeedGenerator.buildPost() -> call several times in a loop
+        
+        return
+
+
+class SortDate:
+    #list of all days from the feed
+    def sortDate(dayList: list):
+
+        return
+
+
+class postImage:
+    postImage = False
+
+class videClip:
+    videoClip = False
+    
+
+class textPost:
+    textPost = False
+    
+
+class Post:
+    
+    engangementScore =0
+    timeStamp = None
+    category = None
+    seen = False
+    
+
+class Engagement:
+    def sortEngagementScore(engagementScore: int):
+        return
+    
+    
+
+class TimeStamp:
+    
+    def markTimeStamp():
+        #mark time stamp of this function being called
+        timeStamp = datetime.now()
+        #from datetime.datetime to string to number
+        day = int(timeStamp.strftime("%d"))
+        hour = int(timeStamp.strftime("%H"))
+        minute = int(timeStamp.strftime("%M"))
+        second = int(timeStamp.strftime("%S"))
+        full = timeStamp.strftime("%H%/%M")
+        stamp = [day, hour, minute, second]
+        return stamp
+
+class Category:
+    def sortCategory(categoryName: category):
+        return
+
+
+def main():
+    return
+    
+ 
+    
+
+        
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
